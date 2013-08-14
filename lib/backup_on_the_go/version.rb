f1dfb04b937c960f5c9da16424fcb187bda5d573
Hong Xu <hong@topbug.net>
2013-08-14 01:03:38 -0700

    Bump version number to 0.2.0.

--- lib/backup_on_the_go/version.rb
+++ lib/backup_on_the_go/version.rb
@@ -3,8 +3,8 @@
 module BackupOnTheGo #:nodoc:#
   module VERSION
     MAJOR = 0
-    MINOR = 1
-    TINY  = 5
+    MINOR = 2
+    TINY  = 0
 
     STRING = [MAJOR, MINOR, TINY].compact.join('.')
   end
